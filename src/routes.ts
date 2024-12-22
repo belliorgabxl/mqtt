006374332dd13e1471beba7727a4782e07b977fb
patarajarin <patara1919@gmail.com>
2024-12-22 23:47:02 +0700

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+export const publicRoutes = [
+    '/',
+    '/documents',
+    '/aboutus',
+    '/production'
+];
+export const authRoutes = [
+    '/login',
+    '/register'
+]
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
@@ -15,4 +25,5 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
